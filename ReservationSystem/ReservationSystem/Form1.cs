0cf7c41a635f5a9f51cc6a51b6dc572e6ed6729f
AtakanTurgut <77547962+AtakanTurgut@users.noreply.github.com>
2024-03-12 17:49:09 +0300

    ADO.NET has been implemented.

---
+++ ReservationSystem/ReservationSystem/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ReservationSystem
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStatusDefinitions_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            Forms.Definitions.FrmStatus FrmStatus = new Forms.Definitions.FrmStatus();
+            FrmStatus.Show();
+        }
+    }
+}
